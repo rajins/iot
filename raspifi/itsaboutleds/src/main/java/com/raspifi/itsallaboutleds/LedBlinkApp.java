6fff5d9924b6c178f379a9b2fc243445977b1ece
Rajin Sasi <sasi.rajinkumar@gmail.com>
2017-07-23 01:21:35 +0530

    initial setup for raspberry pi and led examples

---
+++ raspifi/itsaboutleds/src/main/java/com/raspifi/itsallaboutleds/LedBlinkApp.java
@@ -0,0 +1,12 @@
+package com.raspifi.itsallaboutleds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedBlinkApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedBlinkApp.class, args);
+    }
+}
---
+++ raspifi/itsaboutleds/src/main/java/com/raspifi/itsallaboutleds/controller/LedBlinkController.java
@@ -0,0 +1,26 @@
+package com.raspifi.itsallaboutleds.controller;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LedBlinkController {
+
+    private static GpioPinDigitalOutput pin;
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello, World!";
+    }
+
+    @RequestMapping("/light")
+    public String light() {
+        if (pin == null) {
+            GpioController controller = GpioFactory.getInstance();
+            pin = controller.provisionDigitalOutputPin(RaspiPin.GPIO_01, "My LED", PinState.LOW);
+        }
+        pin.toggle();
+        return "OK";
+    }
+}
